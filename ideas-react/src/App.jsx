1e34c01de70ef8e23126ee6beb82e8b0a10f4dd4
Purple-Code-sh <purple.code.sh@gmail.com>
2025-02-04 09:48:54 -0600

    first idea

---
+++ ideas-react/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen bg-gradient-to-r from-pink-500 via-pink-400 to-orange-500 bg-[length:300%] animate-(--gradientBgAnimation)">
+        
+      <h1 className=" text-6xl font-bold">Animate Your Gradients</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
